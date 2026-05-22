5dc70c5dd3639114fa6a6bbbc8dcca59989b8497
KoTeuKaSeeker <emaillit8@gmail.com>
2026-05-22 22:24:38 +0400

    fix minor compatabilaty bugs

--- renderer/transform.cpp
+++ renderer/transform.cpp
@@ -9,7 +9,7 @@ glm::mat4 Transform::get_model_matrix() const {
     LOG_METHOD();
 
     glm::mat4 T = glm::translate(glm::mat4(1.0f), position);
-    glm::mat4 R = glm::toMat4(glm::normalize(rotation));
+    glm::mat4 R = glm::mat4_cast(glm::normalize(rotation));
     glm::mat4 S = glm::scale(glm::mat4(1.0f), scale);
     return T * R * S;
 }
