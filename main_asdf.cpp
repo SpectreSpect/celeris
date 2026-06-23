f2a81cad61f2b4659e869a857d658f41d45c7e31
SpectreSpect <spectrefromthefuture@gmail.com>
2026-06-24 01:06:11 +0400

    main.cpp now works correctly. The bug is fixed :)

---
+++ main_asdf.cpp
@@ -0,0 +1,262 @@
+#include "vulkan_self/vulkan_engine.h"
+#include "vulkan_self/vulkan_buffer.h"
+#include "vulkan_self/vulkan_resource_loader.h"
+#include "path_utils.h"
+#include "renderer/resources/frame_resources.h"
+#include "camera/camera.h"
+#include "camera/controllers/fps_camera_controller.h"
+#include "renderer/render_object.h"
+#include "managers/shader_manager.h"
+#include "managers/material_manager.h"
+#include "managers/compute_pass_manager.h"
+#include "managers/texture_manager.h"
+#include "managers/material_instance_manager.h"
+#include "renderer/renderer.h"
+#include "managers/mesh_manager.h"
+#include "managers/manager_bundle.h"
+#include "renderer/scene.h"
+#include "renderer/skybox.h"
+#include "renderer/lighting_system/lighting_system.h"
+#include "imgui_layer.h"
+#include "renderer/point_cloud/point_cloud_preprocessor.h"
+#include "renderer/point_cloud/point_cloud.h"
+#include "renderer/point_cloud/lidar/lidar_video.h"
+#include "renderer/point_cloud/gicp/gicp_pass.h"
+#include "renderer/point_cloud/gicp/voxel_point_map.h"
+#include "renderer/point_cloud/gicp/voxel_map_point_inserter.h"
+#include "renderer/point_cloud/gicp/voxel_map_point_reseter.h"
+
+#include <cmath>
+#include <vector>
+
+float srgb_to_linear(float c) {
+    if (c <= 0.04045f) {
+        return c / 12.92f;
+    }
+
+    return std::pow((c + 0.055f) / 1.055f, 2.4f);
+}
+
+VkClearValue make_clear_color_srgb(glm::vec4 color) {
+    VkClearValue clear_color{};
+
+    clear_color.color.float32[0] = srgb_to_linear(color.r);
+    clear_color.color.float32[1] = srgb_to_linear(color.g);
+    clear_color.color.float32[2] = srgb_to_linear(color.b);
+    clear_color.color.float32[3] = color.a;
+
+    return clear_color;
+}
+
+VkClearValue clear_color = make_clear_color_srgb({0.05f, 0.05f, 0.05f, 1.0f});
+
+int main() {
+    GlfwContext glfw_context;
+    Window window(glfw_context, 1280, 720, "Vulkan engine");
+
+    QueueRequest queue_request;
+    queue_request.graphics_count = 2;
+    queue_request.present_count = 1;
+    queue_request.compute_count = 1;
+
+    VulkanEngine engine(glfw_context, window, queue_request);
+
+    UI ui(window, engine);
+    Camera camera;
+    FPSCameraController camera_controller(camera);
+    camera_controller.speed = 20;
+
+    VulkanResourceLoader resource_loader(engine, 154217728);
+
+    ShaderManager shader_manager(engine.device());
+    ComputePassManager compute_pass_manager(engine.device(), shader_manager);
+    TextureManager texture_manager(engine, resource_loader, compute_pass_manager);
+
+    LightingSystem lighting_system(engine, compute_pass_manager);
+    FrameResources frame_resources(engine, lighting_system, engine.num_frames_in_flight());
+    lighting_system.set_frame_resources(frame_resources);
+
+    MaterialManager material_manager(engine, shader_manager, frame_resources);
+    MaterialInstanceManager material_instance_manager(engine, material_manager, texture_manager);
+    MeshManager mesh_manager(engine, resource_loader);
+    ManagerBundle manager_bundle(engine, shader_manager, texture_manager, material_manager,
+                                 material_instance_manager, mesh_manager, compute_pass_manager);
+
+    PointCloudPreprocessor point_cloud_preprocessor(
+        engine.device(),
+        engine.compute_queue(),
+        compute_pass_manager
+    );
+
+    GICPPass gicp_pass(engine, compute_pass_manager);
+    VoxelMapPointInserter voxel_map_inserter(engine, compute_pass_manager);
+    VoxelMapPointReseter voxel_map_reseter(engine, compute_pass_manager);
+
+    Renderer renderer(engine, frame_resources);
+
+    RenderObject unlit_cube(mesh_manager.cube, material_instance_manager.dirt_blinn_phong);
+    RenderObject unlit_cube2(mesh_manager.cube, material_instance_manager.dirt_blinn_phong);
+    RenderObject unlit_cube3(mesh_manager.cube, material_instance_manager.rock_blinn_phong);
+    RenderObject unlit_cube4(mesh_manager.cube, material_instance_manager.unlit);
+
+    const float skybox_exposure = 1.8f;
+    Skybox skybox(
+        mesh_manager.skybox_cube,
+        material_instance_manager.st_peters_square_night_4k_hdr,
+        texture_manager,
+        material_manager.pbr_mp,
+        TextureManager::st_peters_square_night_4k_pbr_map_id,
+        skybox_exposure
+    );
+
+    LidarVideo lidar_video(
+        manager_bundle,
+        point_cloud_preprocessor,
+        "/home/spectre/TEMP_lidar_output_mesh/recording_16/index.csv",
+        0,
+        100
+    );
+
+    glm::vec3 first_position = lidar_video.get_scan(0).point_cloud().transform.position;
+    glm::quat first_rotation = glm::normalize(lidar_video.get_scan(0).point_cloud().transform.rotation);
+
+    for (int i = static_cast<int>(lidar_video.get_scan_count()) - 1; i >= 1; --i) {
+        glm::vec3 p_prev = lidar_video.get_scan(i - 1).point_cloud().transform.position;
+        glm::vec3 p_curr = lidar_video.get_scan(i).point_cloud().transform.position;
+
+        glm::quat q_prev = glm::normalize(lidar_video.get_scan(i - 1).point_cloud().transform.rotation);
+        glm::quat q_curr = glm::normalize(lidar_video.get_scan(i).point_cloud().transform.rotation);
+
+        if (glm::dot(q_prev, q_curr) < 0.0f) {
+            q_curr = -q_curr;
+        }
+
+        glm::vec3 delta_position = p_curr - p_prev;
+        glm::quat delta_rotation = glm::normalize(q_curr * glm::inverse(q_prev));
+
+        lidar_video.get_scan(i).point_cloud().transform.position = delta_position;
+        lidar_video.get_scan(i).point_cloud().transform.rotation = delta_rotation;
+    }
+
+    lidar_video.get_scan(0).point_cloud().transform.position = glm::vec3(0.0f);
+    lidar_video.get_scan(0).point_cloud().transform.rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+
+    VoxelPointMap voxel_point_map(engine, 1500000, 1500000);
+    voxel_map_reseter.reset(voxel_point_map);
+
+    voxel_map_inserter.insert(voxel_point_map, lidar_video.get_scan(0).point_cloud(), lidar_video.get_scan(0).normal_buffer());
+
+    PointCloud voxel_map_point_cloud(manager_bundle, voxel_point_map.map_point_buffer, voxel_point_map.m_map_point_count);
+
+    unlit_cube.set_material_data<BlinPhongMaterialData>({glm::vec4(0.1, 1, 0.5, 32.0), glm::vec4(1, 1, 1, 1)});
+    unlit_cube2.set_material_data<BlinPhongMaterialData>({glm::vec4(0.1, 1, 0.5, 32.0), glm::vec4(1, 1, 1, 1)});
+    unlit_cube3.set_material_data<BlinPhongMaterialData>({glm::vec4(0.1, 1, 0.5, 32.0), glm::vec4(1, 1, 1, 1)});
+    unlit_cube4.set_material_data<UnlitMaterialData>({glm::vec4(0, 1, 1, 1)});
+
+    unlit_cube.transform.position.x = 0;
+    unlit_cube2.transform.position.x = 2;
+    unlit_cube3.transform.position.x = 4;
+    unlit_cube4.transform.position.x = 6;
+
+    unlit_cube.add_child(unlit_cube2);
+    unlit_cube2.add_child(unlit_cube3);
+    unlit_cube3.add_child(unlit_cube4);
+
+    Scene scene;
+    scene.add(skybox);
+    scene.add(voxel_map_point_cloud);
+    skybox.update(scene);
+
+    bool g_pressed = false;
+    bool n_pressed = false;
+
+    int step = 0;
+
+    float last_frame_time = 0.0f;
+    float start_time = static_cast<float>(glfwGetTime());
+    float timer = 0;
+    while (!engine.window().should_close()) {
+        engine.window().poll_events();
+
+        float current_frame_time = static_cast<float>(glfwGetTime()) - start_time;
+        float delta_time = current_frame_time - last_frame_time;
+        last_frame_time = current_frame_time;
+        timer += delta_time;
+
+        uint32_t image_index = 0;
+        if (!engine.aquire_free_resources(image_index)) continue;
+        VulkanCommandBuffer& command_buffer = engine.get_active_command_buffer();
+
+        camera_controller.update(window, delta_time);
+        frame_resources.update_camera(engine.current_frame(), window, camera);
+        lighting_system.update(engine.current_frame(), window, camera);
+
+        if (!g_pressed && glfwGetKey(window.handle(), GLFW_KEY_G) == GLFW_PRESS) {
+            g_pressed = true;
+
+            uint32_t current_frame_id = lidar_video.current_frame_id();
+
+            if (current_frame_id > 0) {
+                LidarScan& current_scan = lidar_video.get_scan(current_frame_id);
+                gicp_pass.step(voxel_point_map, current_scan.point_cloud(), current_scan.normal_buffer());
+            }
+
+            step++;
+        }
+
+        if (g_pressed && glfwGetKey(window.handle(), GLFW_KEY_G) == GLFW_RELEASE) {
+            g_pressed = false;
+        }
+
+        if (!n_pressed && glfwGetKey(window.handle(), GLFW_KEY_N) == GLFW_PRESS) {
+            n_pressed = true;
+
+            uint32_t current_frame_id = lidar_video.current_frame_id();
+
+            if (current_frame_id > 0) {
+                LidarScan& current_scan = lidar_video.get_scan(current_frame_id);
+                LidarScan& previous_scan = lidar_video.get_scan(current_frame_id - 1);
+
+                PointCloud& current_point_cloud = current_scan.point_cloud();
+                PointCloud& previous_point_cloud = previous_scan.point_cloud();
+
+                current_point_cloud.transform.position = previous_point_cloud.transform.position + current_point_cloud.transform.position;
+                current_point_cloud.transform.rotation = glm::normalize(current_point_cloud.transform.rotation * previous_point_cloud.transform.rotation);
+
+                gicp_pass.fit(voxel_point_map, current_scan.point_cloud(), current_scan.normal_buffer(), 10);
+
+                voxel_map_inserter.insert(voxel_point_map, current_scan.point_cloud(), current_scan.normal_buffer());
+                voxel_map_point_cloud.set_instance_view(voxel_point_map.get_map_instance_view());
+            }
+
+            lidar_video.next_frame();
+        }
+
+        if (n_pressed && glfwGetKey(window.handle(), GLFW_KEY_N) == GLFW_RELEASE) {
+            n_pressed = false;
+        }
+
+        {
+            auto command_buffer_scope = command_buffer.begin_scope();
+            {
+                auto render_pass_scope = engine.swapchain_resources().render_pass.begin_scope(
+                    command_buffer,
+                    engine.swapchain_resources().framebuffers[image_index],
+                    engine.swapchain_resources().swapchain,
+                    clear_color
+                );
+
+                renderer.render(command_buffer, scene);
+
+                ui.begin_frame();
+                ui.update_mouse_mode(window);
+                ui.end_frame(command_buffer);
+            }
+        }
+
+        engine.submit_graphic_commands(image_index);
+        engine.present(image_index);
+    }
+
+    engine.device().wait_idle();
+}
