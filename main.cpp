4d3af57664ec377452bb5f6e804b984fdd66dd59
KoTeuKaSeeker <emaillit8@gmail.com>
2026-06-26 15:19:13 +0400

    Change inflation size

--- main.cpp
+++ main.cpp
@@ -161,7 +161,7 @@ int main() {
         .render_distance = chunk_size.x * voxel_size.x * 30,
         .generation_distance = 5,
         .max_write_count = chunk_size.x * chunk_size.y * chunk_size.z * static_cast<uint32_t>(2'000),
-        .inflation_size = 4u,
+        .inflation_size = 1u,
         .car_height_voxels = 3u,
         .display_inflated_voxels = 0u,
         .inflated_voxel_color = 0xFF0707FFu, // 0xFF3355FFu
